12e7eb30a7b0ace0c6574a649faf596d722bde8b
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2023-04-28 11:30:37 +0200

    add an option to approve pages only for specific namespaces. Multiple namespaces can be set pipe separated. Syntax: php maintenance/approveAllPages.php --namespaces "News|Team"

--- maintenance/approveAllPages.php
+++ maintenance/approveAllPages.php
@@ -35,9 +35,14 @@ if ( !isset( $IP ) ) {
 	}
 }
 
+use MediaWiki\MediaWikiServices;
+
 require_once "$IP/maintenance/Maintenance.php";
 
 class ApproveAllPages extends Maintenance {
+
+	/** @var array|false */
+	public $nsFilter = false;
 
 	public function __construct() {
 		parent::__construct();
@@ -48,6 +53,13 @@ class ApproveAllPages extends Maintenance {
 			"force", "Approve the latest version, even if an earlier "
 			. "revision of the page has already been approved."
 		);
+		$this->addOption(
+			"namespaces",
+			"Only approve pages from namespaces belonging "
+			. "to the list of pipe-separated namespace names or namespace indexes",
+			false,
+			true
+		);
 
 		if ( method_exists( $this, 'requireExtension' ) ) {
 			$this->requireExtension( 'Approved Revs' );
@@ -57,6 +69,10 @@ class ApproveAllPages extends Maintenance {
 	public function execute() {
 		global $wgTitle;
 		global $wgEnotifWatchlist;
+
+		if ( $this->hasOption( 'namespaces' ) ) {
+			$this->setNsfilter( explode( '|', $this->getOption( 'namespaces' ) ) );
+		}
 
 		// Don't send out any notifications about people's watch lists.
 		$wgEnotifWatchlist = false;
@@ -75,6 +91,13 @@ class ApproveAllPages extends Maintenance {
 
 		foreach ( $pages as $page ) {
 			$title = Title::newFromID( $page->page_id );
+
+			if ( $this->nsFilter ) {
+				if ( !in_array($title->getNamespace(), $this->nsFilter )) {
+					continue;
+				}
+			}
+
 			// Some extensions, like Page Forms, need $wgTitle
 			// set as well for these checks.
 			$wgTitle = $title;
@@ -107,6 +130,28 @@ class ApproveAllPages extends Maintenance {
 
 		$this->output( "\n Finished setting all current " .
 			"revisions to approved. \n" );
+	}
+
+	private function setNsfilter( array $namespaces ) {
+		if ( count( $namespaces ) == 0 ) {
+			$this->nsFilter = false;
+
+			return;
+		}
+		$this->nsFilter = array_unique( array_map( [ $this, 'getNsIndex' ], $namespaces ) );
+	}
+
+	private function getNsIndex( $namespace ) {
+		$contLang = MediaWikiServices::getInstance()->getContentLanguage();
+		$result = $contLang->getNsIndex( $namespace );
+		if ( $result !== false ) {
+			return $result;
+		}
+		$ns = intval( $namespace );
+		if ( strval( $ns ) === $namespace && $contLang->getNsText( $ns ) !== false ) {
+			return $ns;
+		}
+		$this->fatalError( "Unknown namespace text / index specified: $namespace" );
 	}
 
 }
